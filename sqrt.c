bed39ac458620e3d76c55ddfc67ba61f5138222f
Dilshaad-B18041 <dilshaadmuthalif@ymail.com>
2020-10-15 19:38:21 +0530

    Made changes to achieve negative support

--- sqrt.c
+++ sqrt.c
@@ -6,29 +6,48 @@ positive number. */
 #include <math.h>
 
 // Comment before main()
-int main(int argc, char* argv[]) {
-	
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+
+	if (argc != 2)
+	{
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
-	char *ch = argv[1];
+	char *ch = argv[1], *ch1;
+	ch1 = ch;
+	ch++;
 	int flag = 1;
-	while(*ch!='\0'){
-		if(!(*ch>=48 && *ch<=57)){
+	while (*ch != '\0')
+	{
+		if (!(*ch >= 48 && *ch <= 57))
+		{
 			flag = -1;
 			break;
-		}	
-		ch++;		
+		}
+		ch++;
 	}
-	if(flag==1){
-		int input = atoi(argv[1]);
-		printf("Sqrt of %d is %f\n",input,sqrt(input));
+	if (flag == 1)
+	{
+		int input;
+		if (*ch1 == 45)
+		{
+			argv[1]++;
+			input = atoi(argv[1]);
+			printf("Sqrt of -%d is %fi\n", input, sqrt(input));
+		}
+		else
+		{
+			input = atoi(argv[1]);
+			printf("Sqrt of %d is %f\n", input, sqrt(input));
+		}
 	}
-	else{
-		printf("Input is an invalid number\n");	
+	else
+	{
+		printf("Input is not a valid number\n");
 	}
-	printf("End of program. Exiting.\n");
-	return(0);
+	// Comment added in negativeSupport to check for conflicts
+	printf("End of program exiting...\n");
+	return (0);
 
 } // end main
